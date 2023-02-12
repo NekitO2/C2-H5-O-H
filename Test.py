b5e057d131b0c7daf9389720fd9873629cadc2b9
Nekit_C2_H5_O_H <118076568+NekitO2@users.noreply.github.com>
2023-02-12 09:22:11 +0300

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,27 +1,48 @@
+from random import randint 
+import requests 
 import telebot 
-import wikipedia 
  
 bot = telebot.TeleBot('5891506134:AAHYfMnjRB6hAvuAOvQJYv_J7BEO1ytcKY8') 
-wikipedia.set_lang('ru') 
  
-
-@bot.message_handler(commands = ['help']) 
+def get_images(number): 
  
-def send_help_message(message): 
-    bot.send_message(message.chat.id, 'Отправь мне слово, я отправлю тебе статью Wikipedia') 
+    response = requests.get('https://api.spacexdata.com/v3/launches') 
  
-@bot.message_handler(content_types = ['text']) 
+    response.raise_for_status() 
  
-
-def send_message(message): 
-    try: 
-        article = wikipedia.page(message.text)
-        bot.send_message(message.chat.id, f'Статья про: {article.title}')
-        bot.send_message(message.chat.id, f'{article.content[:1000]}... ') 
-        bot.send_message(message.chat.id, f'Ссылка на полную статью: {article.url}') 
     
-    except: 
+    return  [response.json()[number]['links']['flickr_images'], response.json()[number]['rocket']['rocket_name'], response.json()[number]['mission_name'], response.json()[number]['rocket']['rocket_type'], response.json()[number]['flight_number']]  
+    
+ 
+ 
+@bot.message_handler(commands = ['start']) 
+def send_start_message(message): 
+    bot.send_message(message.chat.id, 'Привет! Отправь мне число от 1 до 100') 
+     
+   
+@bot.message_handler(content_types = ['text'])
+
+def send_images(message):
+
+
+    try:
+    
+        list = get_images(int(message.text)) 
+        # list = get_images(20) 
+
+        bot.send_message(message.chat.id, f'Имя корабля: {list[1]}') 
+        bot.send_message(message.chat.id, f'Тип ракеты: {list[3]}')
+        bot.send_message(message.chat.id, f'Имя миссии: {list[2]}')
+        bot.send_message(message.chat.id, f'Количество полётов: {list[4]}')
         
-        bot.send_message(message.chat.id, 'Я не могу найти эту статью') 
+        if len(list[0]) > 0: 
+            for images in list[0]: 
+                bot.send_message(message.chat.id, images) 
+        else: 
+            bot.send_message(message.chat.id, 'Отправь другое число, тут пусто или нет изображений\n.·´¯`(>▂<)´¯`·. ') 
+
+    except:
+    
+        bot.send_message(message.chat.id, 'Повторяю: введи число от 1 до 100! ') 
  
 bot.polling(non_stop = True)
