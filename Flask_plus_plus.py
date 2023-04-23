4e154b83d0f1e9206711cfe42ffc9bf73c5d9c72
Nekit_C2_H5_O_H <118076568+NekitO2@users.noreply.github.com>
2023-04-23 09:21:23 +0300

    Add files via upload

--- Flask_plus_plus.py
+++ Flask_plus_plus.py
@@ -1,35 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/1')
+@app.route('/')
 
 def first():
-    return '<h1>First page</h1><p>Привет,</p>'
-@app.route('/2')
-
-def second():
-    return '<h1>Second page</h1><p>человек!</p>'
-@app.route('/3')
-
-def third():
-    return '<h1>Third page</h1><p>Ты</p>'
-@app.route('/4')
-
-def fourth():
-    return '<h1>Fourth page</h1><p>же</p>'
-@app.route('/5')
-
-def fifth():
-    return '<h1>Fifth page</h1><p>человек,</p>'
-@app.route('/6')
-
-def sixth():
-    return '<h1>Sixth page</h1><p>верно?</p>'
-
-@app.route('/7')
-
-def seventh():
-    return '<h1>Seventh page</h1><p>......</p>'
+    return render_template('index.html')
 
 app.run()
