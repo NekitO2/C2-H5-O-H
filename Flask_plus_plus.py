71ee2cf27967f2f4d196aa0ab827d653d81af218
Nekit_C2_H5_O_H <118076568+NekitO2@users.noreply.github.com>
2023-04-30 12:54:08 +0300

    Add files via upload

--- Flask_plus_plus.py
+++ Flask_plus_plus.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template
 
+link = 'http://127.0.0.1:5000/'
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,4 +9,12 @@ app = Flask(__name__)
 def first():
     return render_template('index.html')
 
-app.run()
+@app.route('/2')
+
+def second():
+    return render_template('test_button.html')
+
+@app.route('/telegram_bot')
+def telegram_bot():
+    return render_template('telegram_bot.html')
+app.run()           
